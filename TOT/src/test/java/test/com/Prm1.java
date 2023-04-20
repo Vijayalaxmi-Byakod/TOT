94ccc3ddc065b324807693638f0128605cd55f38
Sanganagouda Biradar <Sanganagouda Biradar@LAPTOP-DT4QU3AG.Optilink>
2023-04-20 12:08:30 +0530

    added new code.

---
+++ TOT/src/test/java/test/com/Prm1.java
@@ -0,0 +1,12 @@
+package test.com;
+
+public class Prm1 {
+public static void main(String[] args) {
+	System.out.println("Helllo");
+	System.out.println("Altered");
+
+	System.out.println("Altered 2nd time");
+	System.out.println("Good Time");
+
+}
+}
